2b6416c6dc9178751226502ba1b08eba765db713
itsmeJAYonyt <165497766+itsmeJAYonyt@users.noreply.github.com>
2024-04-27 00:15:30 +0300

    bot.py

---
+++ bot.py
@@ -0,0 +1,22 @@
+@client.event
+async def on_ready():
+    print(f'{client.user} olarak giriş yaptık.')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$merhaba'):
+        await message.channel.send("Selam!")
+    elif message.content.startswith('$bye'):
+        await message.channel.send("\\U0001f642")
+    else:
+        await message.channel.send(message.content)
+    act_cute
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+
+client.run("TOKEN BURAYA")
